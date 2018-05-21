1f3a2d6ce091c27e76132667450a95ba39f3a0b3
huangwenboha563 <37763496@qq.com>
2018-05-21 12:30:12 +0800

    公司电脑提交

---
+++ vue-self/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>你好吗我很好
+
+    <span>fd f</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .hello{
+    width: 100px;
+    height: 500px;
+    h1 {
+      color: blue;
+      span {
+        font-size: 400px;
+      }
+    }
+  }
+
+</style>
