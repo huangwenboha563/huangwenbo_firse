218c2cfc28c6bbf7e1ef03631b291a5968c25f05
huangwenboha563 <37763496@qq.com>
2019-09-27 10:30:57 +0800

    提交旅游网站项目

--- vue-self/travel/src/main.js
+++ vue-self/travel/src/main.js
@@ -5,6 +5,7 @@ import App from './App' // 不写.vue后缀可以
 import router from './router' // 会引入当前路径下的 router文件夹下的index.js
 // 轮播库
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+// vuex
 import store from './store'
 // fastclick
 import fastClick from 'fastclick'
@@ -31,3 +32,14 @@ new Vue({
   components: {App: App}, // 1.局部组件通过bable或者webpack使用es6模块； 2.es6简写为：App
   template: '<App></App>' // 把 App这个局部组件渲染在页面上
 });
+/*
+* state 里面就是存放的饿哦们上面所提到的状态
+* mutations就是存放如何更改状态
+* getters就是从state中派生出状态，比如将state中的某个状态进行过滤后获取新的状态
+* actions就是mutation的加强版，它可以通过commit mutations中的方法来改变状态，最重要的是它可以进行异步操作
+* modules顾名思义，就是当用这容器来来装这些状态还是显得混乱的时候，我们可以把容器分成几块，把状态和管理规则分类来装。这和我们创建js模块是一个目的，让代码结构更清晰。
+*
+*
+*
+*
+* */
--- vue-self/travel/src/store/index.js
+++ vue-self/travel/src/store/index.js
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex);
+// 导出来的是一个Vuex的实例
 export default new Vuex.Store({
   state: { // 存放公用数据
     city: '稷山'
@@ -18,5 +19,18 @@ export default new Vuex.Store({
     changeCity (state, city) {
       state.city = city;
     }
-  }
+  },
+  getters:{}
 });
+
+/*
+* 每一个Vuex的核心就是store（仓库）。
+* store基本上就是一个容器
+* 再次强调，我们通过提交mutation的方式，而非直接改变store.state.count,是因为我们想要更明确地追踪到状态的变化，这个简单的约定能够让你的意图更加明显，
+* Vuex使用单一状态树，是的用一个对象就包含了全部的应用层级状态。至此它便作为一个唯一数据源而存在，这也意味着每个应用将仅仅包含一个store实例。单一状态树让我们能够直接定位任一特定的状态判断，
+* 在调试的过程中也能轻易地取得整个当前应用状态的快照。
+* 单状态树和模块化并不冲突，在后面的章节中，我们会讨论如何将状态和状态变更事件分布到各个子模块中。
+*
+*
+*
+*/
