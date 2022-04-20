763855d3a513a32ddeafcc26824990c4340c7db0
hwb <37763496@qq.com>
2022-04-20 13:14:22 +0800

    vs-code提价

---
+++ vue3Pro/hello-world/src/myInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/person/小吕">查询人员信息</router-link>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <router-link to="/person/刘宽">查询人员信息</router-link>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <router-link to="/base?name=刘阔">查询基本信息</router-link>
+
+
+    <my-input></my-input>
+
+
+  </div>
+  <!--单页面应用要占的坑位-->
+  <router-view></router-view>
+</template>
+<script>
+  import MyInput from './myInput.vue'
+  export default {
+    name: "App",
+    components: {
+      MyInput
+    },
+    computed:{
+
+    }
+  }
+</script>
+<style lang="less">
+
+</style>
--- 李炎恢/base.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by huangwenbo on 18/7/28.
- */
-console.log(1);
-console.log(2)
