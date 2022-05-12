02314661fbbe80d87645d49e7e664ea9e290caa4
huangwenboHisign <wenbo686>
2022-05-12 17:32:51 +0800

    ✨ 测试undefined错误

--- vue3Pro/hello-world/src/components/HelloWorld.vue
+++ vue3Pro/hello-world/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h2>reactive使用中遇到的问题</h2>
+    <div class="myColor">你好吗</div>
     <ul>
       <li v-for="item in testData.data?.real">{{ item.name }}</li>
     </ul>
@@ -27,4 +28,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+@width: 0.8;
+.val(@param) {
+      font-size: @param;
+      color:green;
+    }
+.myColor {
+  // 80%
+  width: percentage(@width);
+  .val(20px)
+}
 </style>
